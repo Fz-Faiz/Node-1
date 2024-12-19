3b93ce1a5508b8670162bed4ee6cd8f39efb899b
Faiz Ahmad <faizahmad43430@gmail.com>
2024-12-20 00:50:30 +0530

    third

---
+++ fake.js
@@ -0,0 +1,17 @@
+const data = [ {a:2, b:3}, {a:4, b:5},{a:2, d:3}, { b:5, a:4} ];
+
+
+
+for(var i  =0;i<data.length;i++){
+    if(data[i].b ){
+        data[i].b=-1;
+    }
+}
+console.log(data)
+console.log("hello")
+
+const a= 3;
+
+a = 5;
+
+console.log(a)
